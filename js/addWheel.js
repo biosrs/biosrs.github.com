bde4d687a522b3592fc5335b004e18a293b14dba
biosrs <biosrs-apoxi@outlook.com>
2016-06-13 00:33:28 +0800

    initial commit

---
+++ js/addWheel.js
@@ -0,0 +1,26 @@
+function addWheel(obj, fn) {
+    function fnWheel(ev) {
+        var oEvent = ev || event;
+        var down = false;
+        //chrome IE
+        if (oEvent.wheelDelta) {
+            if (oEvent.wheelDelta < 0) {
+                down = true;
+            } else {
+                down = false;
+            }
+        } else {
+            if (oEvent.detail > 0) {
+                down = true;
+            } else {
+                down = false;
+            }
+        }
+        fn(down);
+        oEvent.preventDefault && oEvent.preventDefault();
+        return false;
+    }
+
+    obj.addEventListener && obj.addEventListener('DOMMouseScroll', fnWheel, false);
+    obj.onmousewheel = fnWheel;
+}
